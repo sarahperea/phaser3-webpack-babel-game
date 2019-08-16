71f06d975e91494058e314d4241c759c56702485
Sarah D. Perea <sdperea.1@gmail.com>
2019-08-16 10:07:10 +0800

    Refactor ProgressBar.js

--- src/classes/ProgressBar.js
+++ src/classes/ProgressBar.js
@@ -8,53 +8,32 @@ export default class ProgressBar {
 
 	init () {
 		this.progressBar = this.scene.add.graphics();
-		this.progressBox = this.scene.add.graphics();
-		this.styleProgressBox();
+		this.progressBox = this.scene.add.graphics()
+			.fillStyle(0x222222, 0.8)
+			.fillRect(240, 270, 320, 50);
 
-	    const halfWidth = this.scene.cameras.main.width / 2;
-	    const halfHeight = this.scene.cameras.main.height / 2;
-
-		this.addLoadingText(halfWidth, halfHeight - 50);
-		this.addPercentText(halfWidth, halfHeight - 5);
-		this.addAssetText(halfWidth, halfHeight + 50);
+		this.loadingText = this.makeText('Loading.......', -50);
+		this.percentText = this.makeText('0%', -5);
+		this.assetText = this.makeText('', 50);
 	}
 
-	styleProgressBox () {
-	    this.progressBox.fillStyle(0x222222, 0.8);
-	    this.progressBox.fillRect(240, 270, 320, 50);
-	}
-
-	addLoadingText (x, y) {
-	    this.loadingText = this.makeText('Loading.....', x, y);
-	    this.loadingText.setOrigin(0.5, 0.5);
-	}
-
-	addPercentText (x, y) {
-	    this.percentText = this.makeText('0%', x, y);
-	    this.percentText.setOrigin(0.5, 0.5);
-	}
-
-	addAssetText (x, y) {
-		this.assetText = this.makeText('', x, y);
-	    this.assetText.setOrigin(0.5, 0.5);
-	}
-
-	makeText (text, x, y) {
+	makeText (text, adj) {
 		return this.scene.make.text({
-	      x: x,
-	      y: y,
+	      x: this.scene.cameras.main.width / 2,
+	      y: this.scene.cameras.main.height / 2 + adj,
 	      text: text,
 	      style: {
 	          font: '18px monospace',
 	          fill: '#ffffff'
 	      }
-	    });
+	    }).setOrigin(0.5, 0.5);
 	};
 
 	getProgress (value) {
-		this.progressBar.clear();
-        this.progressBar.fillStyle(0xffffff, 1);
-        this.progressBar.fillRect(250, 280, 300 * value, 30);
+		this.progressBar
+			.clear()
+        	.fillStyle(0xffffff, 1)
+        	.fillRect(250, 280, 300 * value, 30);
         this.percentText.setText(parseInt(value * 100) + '%');
 	}
 
